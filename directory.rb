a5f3450811d5557375466a07cca1cef7e91d831a
Vanboylan <108574954+Vanboylan@users.noreply.github.com>
2022-08-08 14:19:54 +0100

    added text to explain

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+#firstly we puts the list of students, new line after each due to method.
+#alternative is for us to use puts("string") but we don't need to
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +13,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#then we print these three lines with no new line. If using \n as new line, use double quotes
 print "Overall, we have "
 print 11
 print " great students"
